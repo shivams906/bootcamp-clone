118da65c4394c1005eca4ce734fa6e88008480be
Shivam Sharma <shivams906@gmail.com>
2020-11-30 12:22:33 +0530

    added UserChangeForm for admin interface without tests

--- users/forms.py
+++ users/forms.py
@@ -3,7 +3,9 @@ Contains form classes for User model.
 """
 from django import forms
 from django.contrib.auth import password_validation
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.core.exceptions import ValidationError
+from django.utils.text import gettext_lazy as _
 from .models import User
 
 
@@ -35,3 +37,42 @@ class UserCreationForm(forms.ModelForm):
         if commit:
             user.save()
         return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """
+    A form for changing user information in admin interface.
+    """
+
+    password = ReadOnlyPasswordHashField(
+        label=_("Password"),
+        help_text=_(
+            "Raw passwords are not stored, so there is no way to see this "
+            "user’s password, but you can change the password using "
+            '<a href="{}">this form</a>.'
+        ),
+    )
+
+    class Meta:
+        model = User
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        password = self.fields.get("password")
+        if password:
+            password.help_text = password.help_text.format("../password/")
+        user_permissions = self.fields.get("user_permissions")
+        if user_permissions:
+            user_permissions.queryset = user_permissions.queryset.select_related(
+                "content_type"
+            )
+
+    def clean_password(self):
+        """
+        Cleans the password.
+        """
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial.get("password")
