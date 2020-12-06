25774aba01aa97906258d1f16f450bc1e4263a51
Shivam Sharma <shivams906@gmail.com>
2020-12-06 19:06:14 +0530

    added link to homepage and some css

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="base.html")),
+    path("", TemplateView.as_view(template_name="base.html"), name="home"),
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
 ]
