d84f16d59cc6658340bbace10089ca5cbbd5e623
Shivam Sharma <shivams906@gmail.com>
2021-02-01 20:44:39 +0530

    added postgresql as db backend

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,5 +1,15 @@
-from .local import *
+from .base import *
 
+SECRET_KEY = env(
+    "SECRET_KEY", default="ig3yfalmzd)@-t^j3ze6wiap^ihygqyt6)e4%-xrycvt9)@rbe"
+)
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory",
+    }
+}
 PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.MD5PasswordHasher",
 ]
